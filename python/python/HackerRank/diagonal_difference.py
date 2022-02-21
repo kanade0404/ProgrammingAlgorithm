28812035046e1c978c1b19055f09e7f500b78d33
kanade <melty0404@gmail.com>
2022-02-22 00:45:18 +0900

    HackerRank

---
+++ python/python/HackerRank/diagonal_difference.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    l = len(arr)
+    return abs(sum([arr[i][i] for i in range(l)]) - sum([arr[i][l - i - 1] for i in range(l)]))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ python/tests/HackerRank/test_diagonal-difference.py
@@ -0,0 +1,9 @@
+from python.HackerRank.diagonal_difference import diagonalDifference
+
+
+def test_case_1():
+    assert diagonalDifference([
+        [11, 2, 4],
+        [4, 5, 6],
+        [10, 8, -12]
+    ]) == 15
