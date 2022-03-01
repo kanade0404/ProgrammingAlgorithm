d60aa95d4a13de02643faa9821c6ac6a3b665036
kanade <melty0404@gmail.com>
2022-03-01 22:01:00 +0900

    HackerRank start solve

---
+++ python/tests/HackerRank/test_mini_max_sum.py
@@ -0,0 +1,9 @@
+from python.HackerRank.mini_max_sum import miniMaxSum
+
+
+def test_case_1():
+    assert miniMaxSum([1 ,3, 5, 7, 9]) == [16, 24]
+
+
+def test_case_2():
+    assert miniMaxSum([1, 2, 3, 4, 5]) == [10, 24]
