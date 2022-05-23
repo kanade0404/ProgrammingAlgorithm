946a8e7c1efe92bc7a563e7d739bd82a6da4d62d
kanade0404 <melty0404@gmail.com>
2022-05-23 19:53:00 +0900

    Partitioning Into Minimum Number Of Deci-Binary Numbers  https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/

--- golang/leetcode/partitioning-into-minimum-number-of-deci-binary-numbers/main.go
+++ golang/leetcode/partitioning-into-minimum-number-of-deci-binary-numbers/main.go
@@ -1,16 +1,18 @@
 package partitioning_into_minimum_number_of_deci_binary_numbers
 
 import (
-	"sort"
 	"strconv"
 	"strings"
 )
 
 func minPartitions(n string) int {
 	arr := strings.Split(n, "")
-	sort.Slice(arr, func(i, j int) bool {
-		return arr[i] > arr[j]
-	})
-	result, _ := strconv.Atoi(arr[0])
+	var result int
+	for _, a := range arr {
+		n, _ := strconv.Atoi(a)
+		if n > result {
+			result = n
+		}
+	}
 	return result
 }
