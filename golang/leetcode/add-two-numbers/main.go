dad6220b551041f79c138985d69aa617474e8d1d
kanade0404 <melty0404@gmail.com>
2023-02-19 10:30:51 +0900

    :sparkles: solve 2. Add Two Numbers

---
+++ golang/leetcode/add-two-numbers/main.go
@@ -0,0 +1,80 @@
+package add_two_numbers
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	r1, l1Len := add(make([]byte, 0, 100), l1, 0)
+	r2, l2Len := add(make([]byte, 0, 100), l2, 0)
+	var (
+		i          int
+		acc1, acc2 byte
+		isCarry    bool
+		result     []byte
+	)
+	for {
+		if i >= l1Len && i >= l2Len {
+			if isCarry {
+				result = append([]byte{1}, result...)
+				i++
+			}
+			break
+		}
+		if i < l1Len {
+			acc1 = r1[i]
+		}
+		if i < l2Len {
+			acc2 = r2[i]
+		}
+		tmp := acc1 + acc2
+		if isCarry {
+			tmp += 1
+		}
+		if tmp > 9 {
+			isCarry = true
+			result = append([]byte{tmp % 10}, result...)
+		} else {
+			isCarry = false
+			result = append([]byte{tmp}, result...)
+		}
+		acc1 = 0
+		acc2 = 0
+		i++
+	}
+	return createNode(result, i, i, nil)
+}
+func add(acc []byte, l *ListNode, count int) ([]byte, int) {
+	if l.Next == nil {
+		return append(acc, byte(l.Val)), count + 1
+	} else {
+		return add(append(acc, byte(l.Val)), l.Next, count+1)
+	}
+}
+func createNode(num []byte, curNumLen, numLen int, l *ListNode) *ListNode {
+	if curNumLen-1 == 0 {
+		return &ListNode{
+			Val:  int(num[0]),
+			Next: l,
+		}
+	} else {
+		if curNumLen == numLen {
+			return createNode(num[1:], curNumLen-1, numLen, &ListNode{
+				Val: int(num[0]),
+			})
+		} else {
+			return createNode(num[1:], curNumLen-1, numLen, &ListNode{
+				Val:  int(num[0]),
+				Next: l,
+			})
+		}
+	}
+}
---
+++ golang/leetcode/add-two-numbers/main_test.go
@@ -0,0 +1,211 @@
+package add_two_numbers
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"testing"
+)
+
+func Test_addTwoNumbers(t *testing.T) {
+	type args struct {
+		l1 *ListNode
+		l2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "test1",
+			args: args{
+				l1: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 4,
+						Next: &ListNode{
+							Val: 3,
+						},
+					},
+				},
+				l2: &ListNode{
+					Val: 5,
+					Next: &ListNode{
+						Val: 6,
+						Next: &ListNode{
+							Val: 4,
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 7,
+				Next: &ListNode{
+					Val: 0,
+					Next: &ListNode{
+						Val: 8,
+					},
+				},
+			},
+		},
+		{
+			name: "test2",
+			args: args{
+				l1: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val: 9,
+								Next: &ListNode{
+									Val: 9,
+									Next: &ListNode{
+										Val: 9,
+										Next: &ListNode{
+											Val: 9,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				l2: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val: 9,
+							},
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 8,
+				Next: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val: 0,
+								Next: &ListNode{
+									Val: 0,
+									Next: &ListNode{
+										Val: 0,
+										Next: &ListNode{
+											Val: 1,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "test3",
+			args: args{
+				l1: &ListNode{
+					Val: 0,
+				},
+				l2: &ListNode{
+					Val: 0,
+				},
+			},
+			want: &ListNode{
+				Val: 0,
+			},
+		},
+		{
+			name: "test4",
+			args: args{
+				l1: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 4,
+						Next: &ListNode{
+							Val: 9,
+						},
+					},
+				},
+				l2: &ListNode{
+					Val: 5,
+					Next: &ListNode{
+						Val: 6,
+						Next: &ListNode{
+							Val: 4,
+							Next: &ListNode{
+								Val: 9,
+							},
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 7,
+				Next: &ListNode{
+					Val: 0,
+					Next: &ListNode{
+						Val: 4,
+						Next: &ListNode{
+							Val: 0,
+							Next: &ListNode{
+								Val: 1,
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "test5",
+			args: args{
+				l1: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 0,
+						Next: &ListNode{
+							Val: 9,
+						},
+					},
+				},
+				l2: &ListNode{
+					Val: 5,
+					Next: &ListNode{
+						Val: 7,
+						Next: &ListNode{
+							Val: 8,
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 6,
+				Next: &ListNode{
+					Val: 7,
+					Next: &ListNode{
+						Val: 7,
+						Next: &ListNode{
+							Val: 1,
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := addTwoNumbers(tt.args.l1, tt.args.l2)
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("(-got+want)\n%v", diff)
+			}
+		})
+	}
+}
